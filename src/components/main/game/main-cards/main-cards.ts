2e753cec38b53d84aa975e955a81b6eee975e0a2
JuliaSemaka <ysemaka@mail.ru>
2021-06-01 00:52:52 +0300

    feat: create timer

--- src/components/main/game/main-cards/main-cards.ts
+++ src/components/main/game/main-cards/main-cards.ts
@@ -4,6 +4,23 @@ import { Card } from './card/card';
 
 const SHOW_TIME = 5;
 
+function startTimer() : void {
+  const timer = document.querySelector('.main-timer');
+  let timeValue = 0;
+
+  function drawTimer() {
+    timeValue += 1000;
+    const d = new Date(timeValue);
+    const strTimer = `0${d.getMinutes()}:${d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds()}`;
+    if (!timer) throw Error('Element not found');
+    timer.innerHTML = strTimer;
+    timer.classList.add('text');
+    timer.classList.add('main-timer__text');
+  }
+  setInterval(() => {
+    drawTimer();
+  }, 1000);
+}
 export class MainCards extends BaseComponent {
   public cards: Card[] = [];
 
@@ -21,6 +38,7 @@ export class MainCards extends BaseComponent {
     this.cards.forEach((card) => this.element.appendChild(card.element));
     setTimeout(() => {
       this.cards.forEach((card) => card.flipToBack());
+      startTimer();
     }, SHOW_TIME * 1000);
   }
 }
--- src/components/main/game/main-timer/main-timer.ts
+++ src/components/main/game/main-timer/main-timer.ts
@@ -5,6 +5,6 @@ export class MainTimer extends BaseComponent {
   constructor() {
     super('div', ['main-timer']);
 
-    this.element.innerHTML = '<p class="text main-timer__text">00:01</p>';
+    this.element.innerHTML = '<p class="text main-timer__text">00:00</p>';
   }
 }
